5482474c48d842b16863a2e02f54d3f789b1f752
luciaburky <luciaburky1@gmail.com>
2023-11-03 14:48:31 -0300

    ohuhu

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/lucia/CLionProjects/ComputacionParalela/main.c" "CMakeFiles/untitled.dir/main.c.obj" "gcc" "CMakeFiles/untitled.dir/main.c.obj.d"
+  "C:/Users/lucia/CLionProjects/ComputacionParalela/cmake-build-debug/mergeNo.c" "CMakeFiles/untitled.dir/mergeNo.c.obj" "gcc" "CMakeFiles/untitled.dir/mergeNo.c.obj.d"
+  "C:/Users/lucia/CLionProjects/ComputacionParalela/cmake-build-debug/mergeParalelo.c" "CMakeFiles/untitled.dir/mergeParalelo.c.obj" "gcc" "CMakeFiles/untitled.dir/mergeParalelo.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/main.c.obj"
+  "CMakeFiles/untitled.dir/main.c.obj.d"
+  "CMakeFiles/untitled.dir/mergeNo.c.obj"
+  "CMakeFiles/untitled.dir/mergeNo.c.obj.d"
+  "CMakeFiles/untitled.dir/mergeParalelo.c.obj"
+  "CMakeFiles/untitled.dir/mergeParalelo.c.obj.d"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
